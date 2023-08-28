3c2a12427e9cc42551da99da3630e500342c5a1b
Avetis Ghukasyan <9576400+AvetisCodes@users.noreply.github.com>
2023-08-27 20:33:18 -0400

    Adding test attribute and adding MSTest unit testing class.

---
+++ SimpleCalculator/SimpleCalculator.UnitTesting.MSTest/UnitTest1.cs
@@ -0,0 +1,11 @@
+namespace SimpleCalculator.UnitTesting.MSTest
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
---
+++ SimpleCalculator/SimpleCalculator/DocumentationAttribute.cs
@@ -0,0 +1,18 @@
+﻿namespace SimpleCalculator;
+
+[AttributeUsage(AttributeTargets.Class)]
+public class DocumentationAttribute : Attribute
+{
+    public string Description { get; set; }
+
+    public DocumentationAttribute(string description) 
+    {
+        Description = description;
+    }
+}
+
+[Documentation("This class does something.")]
+public class MyClass
+{
+
+}
