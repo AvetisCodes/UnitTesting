7a8b3bf3a41ef3839c50c2c298050e2cda6523c2
Avetis Ghukasyan <9576400+AvetisCodes@users.noreply.github.com>
2023-08-29 20:09:43 -0400

    4th and 5th unit test written and setting up 6th.

--- SimpleCalculator/SimpleCalculator.UnitTesting.MSTest/CalculatorServiceTests.cs
+++ SimpleCalculator/SimpleCalculator.UnitTesting.MSTest/CalculatorServiceTests.cs
@@ -29,6 +29,20 @@ namespace SimpleCalculator.UnitTesting.MSTest
         [TestMethod]
         public void Subtract_WithNegativeNumbers_ReturnsCorrectDifference()
         {
+            var result = sut.Subtract(-1, -1);
+            Assert.AreEqual(0, result);
+        }
+
+        [TestMethod]
+        public void Multiply_WithPositiveNumbers_ReturnsSamePositiveProduct()
+        {
+            var result = sut.Multiply(1, 2);
+            Assert.AreEqual(2, result);
+        }
+
+        [TestMethod]
+        public void Multiply_WithPositiveNumbersReversed_ReturnsSamePositiveProduct()
+        {
 
         }
     }
